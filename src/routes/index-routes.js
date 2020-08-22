7700acae9e3987fd0848c33377e4d945ad1f22ae
Rafael Barros <leafar.barros@gmail.com>
2020-08-22 12:01:46 -0300

    commit teste

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.status(200).send({
-    title: 'LocalizationFinderAPI',
+    title: 'LocalizationFinderAPI TESTE',
     version: '1.0.0'
   });
 });
